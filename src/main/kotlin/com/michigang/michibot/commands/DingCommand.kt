345f005c92746207bf09052bab85470e62c41848
Andy <gd_andres98@hotmail.com>
2024-09-10 01:55:48 -0500

    add Dockerfile

--- src/main/kotlin/com/michigang/michibot/commands/DingCommand.kt
+++ src/main/kotlin/com/michigang/michibot/commands/DingCommand.kt
@@ -1,16 +1,20 @@
 package com.michigang.michibot.commands
 
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 
 @Component
 class DingCommand() : SlashCommand {
 
+    private val logger = LoggerFactory.getLogger(DingCommand::class.java)
+
     override val name: String = "ding"
     override fun handle(event: ChatInputInteractionEvent): Mono<Void> =
-            event.reply()
-                    .withContent("Dong!")
-
+            with(event) {
+                logger.info("Sending a Dong!")
+                reply().withContent("Dong!")
+            }
 
 }
