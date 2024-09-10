8847006d8354c3add57c1009758a805ada9e3274
Andy <gd_andres98@hotmail.com>
2024-09-10 00:45:19 -0500

    add bot config

---
+++ src/main/kotlin/com/michigang/michibot/config/BotConfig.kt
@@ -0,0 +1,30 @@
+package com.michigang.michibot.config
+
+import discord4j.core.DiscordClientBuilder
+import discord4j.core.GatewayDiscordClient
+import discord4j.core.`object`.presence.ClientActivity
+import discord4j.core.`object`.presence.ClientPresence
+import discord4j.rest.RestClient
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "bot")
+class BotConfig {
+
+    lateinit var token: String
+
+    @Bean
+    fun gatewayDiscordClient(): GatewayDiscordClient? =
+            DiscordClientBuilder.create(token)
+                    .build()
+                    .gateway()
+                    .setInitialPresence { ClientPresence.online(ClientActivity.listening("tu cotazón uwu")) }
+                    .login()
+                    .block()
+
+    @Bean
+    fun discordRestClient(client: GatewayDiscordClient): RestClient = client.restClient
+
+}
